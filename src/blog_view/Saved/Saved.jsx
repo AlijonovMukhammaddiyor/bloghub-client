8d52b7d34b0922d39af5ed187830992363c7917b
Mukhammaddiyor <mukhammaddiyor@mukhammaddiyor.local>
2021-11-03 12:54:38 +0900

    last changes

--- src/blog_view/Saved/Saved.jsx
+++ src/blog_view/Saved/Saved.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Main from "../../home_view/main/Main";
 import LogNavbar from "../navbar/navbar";
-import Footer from "../Footer/Footer";
 
 export default function SavedOrLiked(props) {
 	return (
